883b06fb6f04237941acd080935d32408e1fff9f
sarahguerreiro <sdmguerre@gmail.com>
2022-07-25 18:08:40 -0300

    sistema de multa

---
+++ aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) {
+    console.log(`Voce ultrapassou o limite de velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de seguranca!`)
---
+++ aula 11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais} vc é`)
+if (pais == 'Brasil') {
+    console.log(`Brasileiro`)
+} else {
+    console.log(`estrangeiro`)
+}
