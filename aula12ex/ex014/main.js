299c89da407786bfaf10aab8ece2303b9b9a8f44
sarahguerreiro <sdmguerre@gmail.com>
2023-02-18 21:37:25 -0300

    chama o objeto img dentro dos blocos condicionais

--- aula12ex/ex014/main.js
+++ aula12ex/ex014/main.js
@@ -6,9 +6,12 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         //bom dia
+        img.src = 'manhazinha.png'
     } else if (hora >= 12 && hora < 18) {
         //boa tarde
+        img.src = 'tardinha.png'
     } else {
         //boa noite
+        img.src = 'noitinha.png'
     }
 }
