3112ecfe38044a39e92f7ad21be345423380f058
sarahguerreiro <sdmguerre@gmail.com>
2023-02-18 21:33:40 -0300

    adc as condicoes pra troca de imagens de acordo com a hora

--- aula12ex/ex014/main.js
+++ aula12ex/ex014/main.js
@@ -4,4 +4,11 @@ function carregar() {
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //bom dia
+    } else if (hora >= 12 && hora < 18) {
+        //boa tarde
+    } else {
+        //boa noite
+    }
 }
