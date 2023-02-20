ed88efef8b96d9d45f6be4a7631544350e4cd455
sarahguerreiro <sdmguerre@gmail.com>
2023-02-20 18:58:34 -0300

    cria uma var pra pegar valores da 2a div

--- aula12ex/ex015/main.js
+++ aula12ex/ex015/main.js
@@ -2,4 +2,5 @@ function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
 }
