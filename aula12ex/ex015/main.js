ab19010ffc456a72f15e9c2e21158549864843db
sarahguerreiro <sdmguerre@gmail.com>
2023-02-20 19:07:37 -0300

    cria um if p verificar se o ano informado ta vazio/ano maior que o ano atual

--- aula12ex/ex015/main.js
+++ aula12ex/ex015/main.js
@@ -3,4 +3,10 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
+
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        window.alert('tudo ok!')
+    }
 }
