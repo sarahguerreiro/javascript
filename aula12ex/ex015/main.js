df9ba41dd85dc89937cc2e5dc5f81d3c54ee86ca
sarahguerreiro <sdmguerre@gmail.com>
2023-02-20 21:09:04 -0300

    toda a logica pra o output sair com imagens de acordo com a idade

--- aula12ex/ex015/main.js
+++ aula12ex/ex015/main.js
@@ -11,12 +11,42 @@ function verificar() {
         var idade = ano - Number(fano.value)
         //res.innerHTML = `Idade Calculada: ${idade}`
         var gen = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
         if (fsex[0].checked) {
             gen = 'homem'
+            if (idade >=0 && idade < 10) {
+                img.setAttribute('src', 'menino.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'moco.png')
+                
+            } else if (idade < 50) {
+                
+                img.setAttribute('src', 'homem.png')
+            } else {
+                
+                img.setAttribute('src', 'idoso.png')
+            }
+            
         } else if (fsex[1].checked) {
             gen = 'mulher'
+            if (idade >=0 && idade < 10) {
+                
+                img.setAttribute('src', 'menina.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'moca.png')
+                
+            } else if (idade < 50) {
+                img.setAttribute('src', 'mulher.png')
+                
+            } else {
+                
+                img.setAttribute('src', 'idosa.png')
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML= `Detectamos ${gen} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
