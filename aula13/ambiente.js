f5485ae47982deef73e41ba35d00da8b2bef67b0
sarahguerreiro <sdmguerre@gmail.com>
2023-02-22 22:16:19 -0300

    organizando a casa para os ex016 e ex017

---
+++ aula13/ambiente.js
@@ -0,0 +1,34 @@
+console.log('vai começar...')
+for(var c = 1; c <= 4; c++){
+    console.log(c)
+}
+console.log('FIM')
+ 
+
+/*
+estrutura de repetição com variavel de controle
+for (var c=1; c <= 10; c++){
+    console.log(`uau${c}`)
+}
+
+estrutura de repetição com teste logico no inicio:
+var c = 1
+while (c <= 6) {
+    console.log(`passo ${c}`)
+    c++
+}
+estrutura de repetição com teste logico no final:
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 6)
+jeito newbie de faer:
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
