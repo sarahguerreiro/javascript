685014201710fc67abb2803f6e58a088812c4990
Sarah <sdmguerre@gmail.com>
2023-12-23 18:11:56 -0300

    cria a class Pessoa e Aluno

---
+++ treinandoJs/estudosPooGuanabara/Aluno.js
@@ -0,0 +1,36 @@
+import { Pessoa } from "./Pessoa.js";
+
+export class Aluno extends Pessoa {
+    nome
+    idade
+    sexo
+    matricula
+    curso
+
+    constructor(nome, idade, sexo, matricula, curso) {
+        super(nome, idade, sexo);
+        this.matricula = matricula;
+        this.curso = curso;
+    }
+
+    cancelarMatricula() {
+        
+    }
+
+    getMatricula() {
+
+    }
+
+    setMatricula() {
+
+    }
+
+    getCurso() {
+
+    }
+    
+    setCurso() {
+
+    }
+
+}
--- treinandoJs/estudosPooGuanabara/Pessoa.js
+++ treinandoJs/estudosPooGuanabara/Pessoa.js
@@ -1,3 +1,39 @@
-class Pessoa {
+export class Pessoa {
+    nome
+    idade
+    sexo
     
+    constructor(nome, idade, sexo){
+        this.nome = nome
+        this.idade = idade
+        this.sexo = sexo
+    }
+
+    fazerAniv() {
+
+    }
+
+    getNome() {
+
+    }
+
+    setNome() {
+
+    }
+
+    getIdade() {
+
+    }
+
+    setIdade() {
+
+    }
+
+    getSexo() {
+
+    }
+    
+    setSexo() {
+
+    }
 }
